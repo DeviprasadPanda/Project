5bf78e0e1bb483e11aa80b640c7f985103bd8b51
DEVI PRASAD PANDA <92011953+DeviprasadPanda@users.noreply.github.com>
2022-08-28 23:35:02 +0530

    Add files via upload

---
+++ valid_mail.py
@@ -0,0 +1,10 @@
+import re
+
+def solve(m):
+   pat = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$" or "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[in]$" or "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[com]$"
+   if re.match(pat,m):
+      return ("Valid")
+   return ("Invalid")
+
+m = input("Enter email for validation ")
+print(solve(m))
